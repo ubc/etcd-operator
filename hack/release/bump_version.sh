5c26cc548b074dff19b3f7336db460f7f6ff877e
Nico Berlee <nico.berlee@on2it.net>
2022-05-16 08:56:20 +0200

    chore: support layer pinning in release script

--- hack/release/bump_version.sh
+++ hack/release/bump_version.sh
@@ -9,8 +9,8 @@ newv=$2
 echo "old version: ${oldv}, new version: ${newv}"
 
 sed -i -e "s/${oldv}+git/${newv}/g" version/version.go
-sed -i -e "s/${oldv}/${newv}/g" example/deployment.yaml
-sed -i -e "s/${oldv}/${newv}/g" example/deployment-ha.yaml
-sed -i -e "s/${oldv}/${newv}/g" example/etcd-backup-operator/deployment.yaml
-sed -i -e "s/${oldv}/${newv}/g" example/etcd-restore-operator/deployment.yaml
+sed -i -e "s/${oldv}.*/${newv}/g" example/deployment.yaml
+sed -i -e "s/${oldv}.*/${newv}/g" example/deployment-ha.yaml
+sed -i -e "s/${oldv}.*/${newv}/g" example/etcd-backup-operator/deployment.yaml
+sed -i -e "s/${oldv}.*/${newv}/g" example/etcd-restore-operator/deployment.yaml
 
